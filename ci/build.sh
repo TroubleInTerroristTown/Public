570e9439f3d1c2297141932bbbeeb7d3543dc29f
Bara <r-bara@live.de>
2017-06-25 09:32:00 +0200

    Now... (looks better)

--- ci/build.sh
+++ ci/build.sh
@@ -30,8 +30,8 @@ do
   addons/sourcemod/scripting/spcomp -E -v0 $file
 done
 
-echo -e "Compile 3rd-party-plugins\n"
-echo -e -e "\nCompiling addons/sourcemod/scripting/CustomPlayerSkins.sp..."
+echo -e "\nCompile 3rd-party-plugins"
+echo -e -e "Compiling addons/sourcemod/scripting/CustomPlayerSkins.sp..."
 addons/sourcemod/scripting/spcomp -E -v0 addons/sourcemod/scripting/CustomPlayerSkins.sp
 echo -e -e "\nCompiling addons/sourcemod/scripting/no_weapon_fix.sp..."
 addons/sourcemod/scripting/spcomp -E -v0 addons/sourcemod/scripting/no_weapon_fix.sp
