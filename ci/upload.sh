abb2c62386e7fb6f39e25639ba5781f25b255c8c
Bara Bara <r-bara@live.de>
2017-08-20 15:32:53 +0200

    Revert "Again fix upload"
    This reverts commit 49fc1f6e7b8bf432a4c2ee9750397845d01b07e9.

--- ci/upload.sh
+++ ci/upload.sh
@@ -2,7 +2,7 @@
 
 git fetch --unshallow
 COUNT=$(git rev-list --count HEAD)
-VERSION=2_1_$COUNT
+VERSION=2.1.$COUNT
 HASH="$(git log --pretty=format:%h -n 1)"
 FILE=ttt-$2-$1-$VERSION-$HASH.zip
 LATEST=ttt-latest-$2-$1.zip
