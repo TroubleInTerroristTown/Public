299492682ff295af5354cd624bb9ed9760fc0dd3
Bara <r-bara@live.de>
2017-11-22 22:58:18 +0100

    Disable warning
    https://github.com/alliedmodders/sourcepawn/blob/350ea3f4cb3f38ad53a11cf475600cfe1ddb9ed6/compiler/sc1.cpp#L1124

--- ci/build.sh
+++ ci/build.sh
@@ -29,11 +29,11 @@ done
 
 echo -e "\nCompile 3rd-party-plugins"
 echo -e "Compiling addons/sourcemod/scripting/CustomPlayerSkins.sp..."
-addons/sourcemod/scripting/spcomp -E -v0 addons/sourcemod/scripting/CustomPlayerSkins.sp
+addons/sourcemod/scripting/spcomp -E -w234 -O2 -v2 addons/sourcemod/scripting/CustomPlayerSkins.sp
 echo -e "\nCompiling addons/sourcemod/scripting/no_weapon_fix.sp..."
-addons/sourcemod/scripting/spcomp -E -v0 addons/sourcemod/scripting/no_weapon_fix.sp
+addons/sourcemod/scripting/spcomp -E -w234 -O2 -v2 addons/sourcemod/scripting/no_weapon_fix.sp
 echo -e "\nCompiling addons/sourcemod/scripting/block_messages.sp..."
-addons/sourcemod/scripting/spcomp -E -v0 addons/sourcemod/scripting/block_messages.sp
+addons/sourcemod/scripting/spcomp -E -w234 -O2 -v2 addons/sourcemod/scripting/block_messages.sp
 
 echo -e "Remove plugins folder if exists\n"
 if [ -d "addons/sourcemod/plugins" ]; then
